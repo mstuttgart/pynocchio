896e94dc2ab5f17f045bcced1d611144942610ba
MStuttgart <michellstut@gmail.com>
2014-10-13 00:30:56 -0300

    [NEW] compile script added.

---
+++ compile_ui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pyside-rcc resource/main_window.qrc -o src/main_window_rc.py
+
+pyside-uic view/main_window.ui -o src/ui_main_window.py
+pyside-uic view/go_to_page_dialog.ui -o src/ui_go_to_page_dialog.py
+pyside-uic view/about_dialog.ui -o src/ui_about_dialog.py
+pyside-uic view/preference_dialog.ui -o src/ui_preference_dialog.py
