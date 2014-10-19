75d796e767062f6d2b7abbcb5a9de23155f572c5
MStuttgart <michellstut@gmail.com>
2014-10-19 02:22:28 -0200

    Update content

--- compile_ui.sh
+++ compile_ui.sh
@@ -6,3 +6,5 @@ pyside-uic view/main_window.ui -o src/ui_main_window.py
 pyside-uic view/go_to_page_dialog.ui -o src/ui_go_to_page_dialog.py
 pyside-uic view/about_dialog.ui -o src/ui_about_dialog.py
 pyside-uic view/preference_dialog.ui -o src/ui_preference_dialog.py
+
+pyside-lupdate pyellow-comic-reader.pro
