d2e1e40eaff937c9284553b1837aac8bcd05ab3d
MStuttgart <michellstut@gmail.com>
2014-10-19 12:29:31 -0200

    ui updates

--- src/ui_go_to_page_dialog.py
+++ src/ui_go_to_page_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'view/go_to_page_dialog.ui'
 #
-# Created: Sun Oct 19 12:24:23 2014
+# Created: Sun Oct 19 12:28:03 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
