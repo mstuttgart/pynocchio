9e851f6f7d8f85f10fcfc6aa8a9d69d471d04396
MStuttgart <michellstut@gmail.com>
2014-12-10 01:00:49 -0200

    Add bookmark implementado

--- src/sqlite_bookmarks.py
+++ src/sqlite_bookmarks.py
@@ -8,33 +8,41 @@ class SQLiteBookmarks(object):
     def __init__(self):
         super(SQLiteBookmarks, self).__init__()
 
-    @staticmethod
-    def _verify_table_exist(conn):
-        cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='BOOKMARK'")
-
-        if cursor.rowcount > 0:
-            return True
-
-        return False
+    # @staticmethod
+    # def _verify_table_exist(conn):
+    #     cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='BOOKMARK'")
+    #
+    #     if cursor is None:
+    #         return True
+    #
+    #     return False
 
     @staticmethod
     def add_bookmark(filename, path, page):
 
         conn = sqlite3.connect('bookmarks.db')
 
-        if SQLiteBookmarks._verify_table_exist(conn) is not True:
-            conn.execute('''CREATE TABLE BOOKMARK
-                   (PATH TEXT PRIMARY KEY     NOT NULL,
-                   NAME           TEXT     NOT NULL,
-                   PAGE           INT);''')
-
-        conn.execute("INSERT INTO BOOKMARK (PATH,NAME,PAGE) \
-        VALUES (path, filename, page)")
+        sql = "CREATE TABLE IF NOT EXISTS BOOKMARK (PATH TEXT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, PAGE INTEGER);"
+        conn.execute(sql)
+        conn.execute("INSERT INTO BOOKMARK VALUES ('%s', '%s', %d)" % (path, filename, page))
 
         conn.commit()
-        print "Records created successfully";
+        print "Records created successfully"
         conn.close()
 
+    @staticmethod
+    def delete_bookmark(path, page):
+
+        conn = sqlite3.connect('bookmarks.db')
+
+        sql = "CREATE TABLE IF NOT EXISTS BOOKMARK (PATH TEXT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, PAGE INTEGER);"
+        conn.execute(sql)
+
+        conn.execute("DELETE FROM BOOKMARK WHERE PATH='%s'" % path)
+
+        conn.commit()
+        print "Records created successfully"
+        conn.close()
 
 
 
