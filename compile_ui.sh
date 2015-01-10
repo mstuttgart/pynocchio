4b691c8c3a6871b6c89e6646b0cf2820d732d140
michell <michellstut@gmail.com>
2015-01-10 13:29:09 -0200

    compile_ui.sh updated

--- compile_ui.sh
+++ compile_ui.sh
@@ -2,10 +2,10 @@
 
 pyside-rcc resource/main_window.qrc -o src/main_window_rc.py
 
-pyside-uic view/main_window.ui -o src/ui_main_window.py
-pyside-uic view/go_to_page_dialog.ui -o src/ui_go_to_page_dialog.py
-pyside-uic view/about_dialog.ui -o src/ui_about_dialog.py
-pyside-uic view/preference_dialog.ui -o src/ui_preference_dialog.py
-pyside-uic view/bookmark_manager_dialog.ui -o src/ui_bookmark_manager_dialog.py
+pyside-uic view/main_window.ui -o src/main_window_ui.py
+pyside-uic view/go_to_page_dialog.ui -o src/go_to_page_dialog_ui.py
+pyside-uic view/about_dialog.ui -o src/about_dialog_ui.py
+pyside-uic view/preference_dialog.ui -o src/preference_dialog_ui.py
+pyside-uic view/bookmark_manager_dialog.ui -o src/bookmark_manager_dialog_ui.py
 
 #pyside-lupdate pyellow-comic-reader.pro
