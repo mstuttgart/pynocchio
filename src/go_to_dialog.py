a607d7c7c67dc5aeffee8168a9dbf3e278cccd4e
michell <michellstut@gmail.com>
2015-01-12 19:26:29 -0200

    [FIX] Bug in open the dialog

--- src/go_to_dialog.py
+++ src/go_to_dialog.py
@@ -12,29 +12,27 @@ class GoToDialog(GoToDialogDialogForm, GoToDialogBase):
         self.model = model
         self.viewer = viewer
 
-        self.dialogHeight = self.height()
-        self.spinBox_go_page = self.spinBox_go_page
-        self.spinBox_go_page.setValue(self.model.get_current_page_index() - 1)
+        # self.height = self.height()
+        # self.spinBox_go_page = self.spinBox_go_page
+        self.spinBox_go_page.setValue(self.model.comic.get_current_page_number())
         self.change_label_image()
 
     def accept(self, *args, **kwargs):
-        super(GoToDialog, self).accept(*args, **kwargs)
-
-        self.model.set_current_page_index(
-            self.spinBox_go_page.value() - 1)
+        self.model.set_current_page_index(self.spinBox_go_page.value() - 1)
         self.viewer.update_view(self.model.get_current_page())
 
+        super(GoToDialog, self).accept(*args, **kwargs)
+
     def rejected(self, *args, **kwargs):
+        self.model.set_current_page_index(int(self.lineEdit_current_page.text()))
         super(GoToDialog, self).rejected(*args, **kwargs)
-        self.model.set_current_page_index(
-            int(self.lineEdit_current_page.text()))
 
     def change_label_image(self):
         self.model.set_current_page_index(self.spinBox_go_page.value() - 1)
 
         image_page = self.model.get_current_page()
         image_page = image_page.scaledToHeight(
-            self.dialogHeight * 0.6, QtCore.Qt.SmoothTransformation)
+            self.height() * 0.6, QtCore.Qt.SmoothTransformation)
 
         self.label_icon.setPixmap(image_page)
 
