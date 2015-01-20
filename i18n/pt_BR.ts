2ecfbfe7cf3c9b9ca9632e33ac5168c933072c8d
michell <michellstut@gmail.com>
2015-01-20 00:25:30 -0200

    Translation folder added

---
+++ i18n/pt_BR.ts
@@ -0,0 +1,427 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="view/about_dialog.ui" line="14"/>
+        <source>About Pynocchio Comic Reader</source>
+        <translation>Sobre Pynocchio Comic Reader</translation>
+    </message>
+    <message>
+        <location filename="view/about_dialog.ui" line="37"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message utf8="true">
+        <location filename="view/about_dialog.ui" line="55"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/icons/freeiconmaker_2 1.png&quot; /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:12pt; font-weight:600; color:#c05800;&quot;&gt;Pynocchio&lt;/span&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:12pt; font-weight:600;&quot;&gt; Comic Reader - Beta&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:11pt; font-weight:600; color:#c05800;&quot;&gt;Pynocchio&lt;/span&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:10pt;&quot;&gt; is an image viewer specifically designed to handle comic books.  &lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:10pt;&quot;&gt;It reads ZIP, RAR and TAR archives, as well as plain image files.&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;;&quot;&gt;Pynocchio Comic Reader is licensed under the GNU General Public License.&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;DejaVu Sans&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;;&quot;&gt;Copyright © 2014 &lt;/span&gt;&lt;a href=&quot;michellstut@gmail.com&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; text-decoration: underline; color:#539fa3;&quot;&gt;Michell Stuttgart Faria&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;;&quot;&gt;Pynocchio use &lt;/span&gt;&lt;a href=&quot;http://freeiconmaker.com/&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; text-decoration: underline; color:#0057ae;&quot;&gt;FREE Icon Maker&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;;&quot;&gt; to build icon set.&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;;&quot;&gt;Icons pack by &lt;/span&gt;&lt;a href=&quot;https://www.iconfinder.com/iconsets/streamline-icon-set-free-pack&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; text-decoration: underline; color:#0057ae;&quot;&gt;Streamline icon set free pack.&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;https://github.com/MStuttgart&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; text-decoration: underline; color:#539fa3;&quot;&gt;Github&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Bookmark_Dialog</name>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="80"/>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="85"/>
+        <source>Path</source>
+        <translation>Diretório</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="90"/>
+        <source>Page</source>
+        <translation>Página</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="110"/>
+        <source>Remover</source>
+        <translation type="obsolete">Remover</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="117"/>
+        <source>Selecionar tudo</source>
+        <translation type="obsolete">Selecionar tudo</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="124"/>
+        <source>Sair</source>
+        <translation type="obsolete">Fechar</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="131"/>
+        <source>Open</source>
+        <translation type="obsolete">Abrir</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="110"/>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="117"/>
+        <source>Select All</source>
+        <translation>Selecionar tudo</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="124"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="view/bookmark_manager_dialog.ui" line="131"/>
+        <source>Load</source>
+        <translation>Carregar</translation>
+    </message>
+</context>
+<context>
+    <name>GoPageDialog</name>
+    <message>
+        <location filename="view/go_to_page_dialog.ui" line="14"/>
+        <source>Go to Page</source>
+        <translation>Ir para a página</translation>
+    </message>
+    <message>
+        <location filename="view/go_to_page_dialog.ui" line="29"/>
+        <source>Page Preview</source>
+        <translation>Visualização </translation>
+    </message>
+    <message>
+        <location filename="view/go_to_page_dialog.ui" line="90"/>
+        <source>Number of pages: </source>
+        <translation>Número de páginas:</translation>
+    </message>
+    <message>
+        <location filename="view/go_to_page_dialog.ui" line="125"/>
+        <source>Current page:        </source>
+        <translation>Página atual:</translation>
+    </message>
+    <message>
+        <location filename="view/go_to_page_dialog.ui" line="148"/>
+        <source>Go to page:           </source>
+        <translation>Ir para a página:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="view/main_window.ui" line="174"/>
+        <source>&amp;File</source>
+        <translation>&amp;Arquivo</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="178"/>
+        <source>&amp;Recent files</source>
+        <translation>A&amp;rquivos recentes</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="189"/>
+        <source>&amp;View</source>
+        <translation>&amp;Visualizar</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="203"/>
+        <source>&amp;Navegation</source>
+        <translation>&amp;Navegação</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="218"/>
+        <source>&amp;Help</source>
+        <translation>A&amp;juda</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="225"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Configuração</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="233"/>
+        <source>&amp;Bookmarks</source>
+        <translation>&amp;Favoritos</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="313"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Abrir</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="331"/>
+        <source>&amp;About Pynocchio</source>
+        <translation>&amp;Sobre o Pynocchio</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="336"/>
+        <source>&amp;About Qt</source>
+        <translation>&amp;Sobre o Qt</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="345"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="360"/>
+        <source>&amp;Next page</source>
+        <translation>&amp;Próxima página</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="363"/>
+        <source>Right</source>
+        <translation>Direita</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="384"/>
+        <source>&amp;Previous page</source>
+        <translation>&amp;Página anterior</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="387"/>
+        <source>Left</source>
+        <translation>Esquerda</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="402"/>
+        <source>&amp;First page</source>
+        <translation>&amp;Primeira página</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="405"/>
+        <source>Ctrl+Left</source>
+        <translation>Ctrl+Esquerda</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="420"/>
+        <source>&amp;Last page</source>
+        <translation>&amp;Última página</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="423"/>
+        <source>Ctrl+Right</source>
+        <translation>Ctrl+Direita</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="441"/>
+        <source>&amp;Rotate left</source>
+        <translation>Rotacionar para esquerda</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="459"/>
+        <source>&amp;Rotate right</source>
+        <translation>Rotacionar para direita</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="483"/>
+        <source>&amp;Vertical adjust</source>
+        <translation>Ajuste vertical</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="507"/>
+        <source>&amp;Horizontal adjust</source>
+        <translation>Ajuste horizontal</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="525"/>
+        <source>&amp;Fullscreen</source>
+        <translation>Tela cheia</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="543"/>
+        <source>&amp;Go to page...</source>
+        <translation>Ir para página...</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="564"/>
+        <source>&amp;Whole page</source>
+        <translation>Página inteira</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="576"/>
+        <source>&amp;Preferences</source>
+        <translation>Preferência</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="594"/>
+        <source>&amp;Original fit</source>
+        <translation>Tamanho original</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="611"/>
+        <source>&amp;Show Toolbar</source>
+        <translation>Exibir barra de ferramentas</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="622"/>
+        <source>S&amp;how Statusbar</source>
+        <translation>Exibir barra de status</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="634"/>
+        <source>&amp;Add bookmark</source>
+        <translation>Adicionar aos favoritos</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="649"/>
+        <source>&amp;Remove bookmark</source>
+        <translation>Remover dos favoritos</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="658"/>
+        <source>Bookmark &amp;manager</source>
+        <translation>Gerenciar favoritos</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="688"/>
+        <source>Next Comic</source>
+        <translation>Próximo quadrinho</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="691"/>
+        <source>Ctrl+Shift+Right</source>
+        <translation>Ctrl+Shift+Direita</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="706"/>
+        <source>Previous Comic</source>
+        <translation>Quadrinho anterior</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="709"/>
+        <source>Ctrl+Shift+Left</source>
+        <translation>Ctrl+Shift+Esquerda</translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="21"/>
+        <source>Pynocchio Comic Reader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="28"/>
+        <source>Pyellow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="264"/>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="316"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="348"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="444"/>
+        <source>Ctrl+Shift+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="462"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="486"/>
+        <source>Ctrl+J</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="510"/>
+        <source>Ctrl+K</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="528"/>
+        <source>F</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="546"/>
+        <source>Ctrl+G</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="567"/>
+        <source>Ctrl+L</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="597"/>
+        <source>Ctrl+H</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="view/main_window.ui" line="667"/>
+        <source>Open &amp;Folder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="src/main_window.py" line="97"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="src/main_window.py" line="97"/>
+        <source>Error to load file </source>
+        <translation>Erro ao carregar arquivo</translation>
+    </message>
+    <message>
+        <location filename="src/main_window.py" line="104"/>
+        <source>Open comic file</source>
+        <translation>Abrir quadrinho</translation>
+    </message>
+    <message>
+        <location filename="src/main_window.py" line="104"/>
+        <source>All supported files (*.zip *.cbz *.rar *.cbr *.tar *.cbt);; Zip Files (*.zip *.cbz);; Rar Files (*.rar *.cbr);; Tar Files (*.tar *.cbt);; All files (*)</source>
+        <translation>Todos os formatos suportados (*.zip *.cbz *.rar *.cbr *.tar *.cbt);; Arquivos Zip (*.zip *.cbz);; Arquivos Rar (*.rar *.cbr);; Arquivos Tar (*.tar *.cbt);; Todos os arquivos (*)</translation>
+    </message>
+    <message>
+        <location filename="src/main_window.py" line="307"/>
+        <source>About Qt</source>
+        <translation>Sobre o Qt</translation>
+    </message>
+</context>
+<context>
+    <name>StatusBar</name>
+    <message>
+        <location filename="src/status_bar.py" line="70"/>
+        <source>Page: </source>
+        <translation>Página:</translation>
+    </message>
+    <message>
+        <location filename="src/status_bar.py" line="78"/>
+        <source>Resolution: </source>
+        <translation>Resolução:</translation>
+    </message>
+    <message>
+        <location filename="src/status_bar.py" line="87"/>
+        <source>Title: </source>
+        <translation>Título:</translation>
+    </message>
+</context>
+</TS>
