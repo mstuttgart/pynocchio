7fdf0ff3cd8fed7560660870e71265cd95f30be3
michell <michellstut@gmail.com>
2015-01-24 15:30:35 -0200

    Color button added

---
+++ src/color_button.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+from PyQt4 import QtGui
+
+
+class ColorButton(QtGui.QPushButton):
+
+    def __init__(self, parent=None):
+        super(ColorButton, self).__init__(parent)
+        self.background_color = QtGui.QColor()
+
+    def paintEvent(self, e):
+        p = QtGui.QPainter(self)
+        p.fillRect(0, 0, self.width(), self.height(), self.background_color)
+        p.end()
+        e.accept()
+
+    def reset_background_color(self):
+        self.background_color = QtGui.QColor()
