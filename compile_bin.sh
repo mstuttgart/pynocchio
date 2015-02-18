4110579dac65e16f4f816677ad371d99f31dd6cc
mstuttgart <michellstut@gmail.com>
2015-02-18 18:06:34 -0200

    bin folder and compile_bin.sh updated

--- compile_bin.sh
+++ compile_bin.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo "Preparing..."
 cp setup.py src/ 
 cd src/
+echo "Build python files..."
 python setup.py build
-mv -i build/exe.linux-x86_64-2.7 ../bin/
+echo "\nMoving binary files to bin folder"
+rm -r ../bin/*
+mv -f build/exe.linux-x86_64-2.7 ../bin/
 rm -r build
 rm setup.py
 cd ../
+echo "Moving ui files to bin folder"
 cp -r view/ bin/
+echo "Moving translation files to bin folder"
 cp -r i18n/ bin
+
+echo "Get the binary in the bin/exe.linux-x86_64-2.7 folder"
