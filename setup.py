dbdc17b9c1b1a3bf535b066472268b7a63743921
gitusername <michellstut@gmail.com>
2015-02-25 17:08:22 -0300

    Remove tags ena add setup.py e MANIFEST

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from distutils.core import setup
-from cx_Freeze import setup, Executable
-exe = Executable(
-    script="main.py",
-    #base="Win32GUI", # Retirar comentario se for contruir um executavel para windows
-    )
+
 setup(
     name='pynocchio-comic-reader',
-    version='1.0.0',
-    # packages=['src'],
-    url='http://mstuttgart.github.io/pynocchio-comic-reader/',
+    version='beta',
+    packages=[''],
+    url='http://pynocchio.github.io/',
     license='GPLv3',
     author='Michell Stuttgart',
-    description='Pynocchio is a image viewer specialized in manga/comic '
-                'reading',
-    # Devemos adicionar os .py que nao sao chamados explicitamente em nosso codigo
-    options={"build_exe": {"includes": ["atexit", "color_button",
-                                        "main_window_rc", "viewer"]}},
-    executables=[exe],
+    author_email='michellstut@gmail.com',
+    description='Comic Reader'
 )
