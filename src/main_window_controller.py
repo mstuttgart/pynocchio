8764685053dec0a018f08f87db348adf8e49e22f
mstuttgart <michellstut@gmail.com>
2015-04-21 15:40:31 -0300

    pyqt.slot removed from methods of controller

--- src/main_window_controller.py
+++ src/main_window_controller.py
@@ -25,80 +25,61 @@ class MainWindowController():
         self.main_window_view = main_window_view.MainWindowView(self)
         self.main_window_model = main_window_model.MainWindowModel(self)
 
-    @QtCore.pyqtSlot()
     def open(self):
         print
 
-    @QtCore.pyqtSlot()
     def save_image(self):
         print
 
-    @QtCore.pyqtSlot()
     def online_comics(self):
         self.main_window_view.setCentralWidget(self.web_view)
         print
 
-    @QtCore.pyqtSlot()
     def next_page(self):
         print
 
-    @QtCore.pyqtSlot()
     def previous_page(self):
         print
 
-    @QtCore.pyqtSlot()
     def first_page(self):
         print
 
-    @QtCore.pyqtSlot()
     def last_page(self):
         print
 
-    @QtCore.pyqtSlot()
     def go_to_page(self):
         print
 
-    @QtCore.pyqtSlot()
     def next_comic(self):
         print
 
-    @QtCore.pyqtSlot()
     def previous_comic(self):
         print
 
-    @QtCore.pyqtSlot()
     def rotate_left(self):
         print
 
-    @QtCore.pyqtSlot()
     def rotate_right(self):
         print
 
-    @QtCore.pyqtSlot()
     def fullscreen(self):
         print
 
-    @QtCore.pyqtSlot()
     def add_bookmark(self):
         print
 
-    @QtCore.pyqtSlot()
     def remove_bookmark(self):
         print
 
-    @QtCore.pyqtSlot()
     def bookmark_manager(self):
         print
 
-    @QtCore.pyqtSlot()
     def show_toolbar(self):
         print
 
-    @QtCore.pyqtSlot()
     def show_statusbar(self):
         print
 
-    @QtCore.pyqtSlot()
     def preference_dialog(self):
         print
 
