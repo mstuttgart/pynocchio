6873a274beffe57389339ac17d677836a3731719
mstuttgart <michellstut@gmail.com>
2015-05-18 23:09:33 -0300

    Adjust in parse method

--- src/path_file_filter.py
+++ src/path_file_filter.py
@@ -23,17 +23,14 @@ from utility import Utility
 
 class PathFileFilter(object):
 
-    def __init__(self, path, extension_list):
-        self._current_path = path
+    def __init__(self, extension_list):
+        self._current_path = None
         self._previous_path = None
         self._next_path = None
         self.extension_list = extension_list
-        self._parse_dir()
+        self.file_list = None
 
     def _parse_dir(self):
-
-        # files = os.listdir(self.current_path)
-        # arquivos = [arq for arq in files if os.path.isfile(arq)]
 
         dir_name = Utility.get_dir_name(self.current_path)
         file_name = Utility.get_base_name(self.current_path)
@@ -42,11 +39,6 @@ class PathFileFilter(object):
         file_list = []
         for ext in self.extension_list:
             file_list += glob.glob1(dir_name, ext)
-            print file_list
-
-        # get files with extension stored in ext
-        # for ext in self.extension_list:
-        #     file_list += [arq for arq in arquivos if arq.lower().endswith(ext)]
 
         # current dile index list
         current_index = file_list.index(file_name)
@@ -63,12 +55,23 @@ class PathFileFilter(object):
         else:
             self._previous_path = None
 
-    def parse(self, path, extension_list):
+        return file_list
+
+    def parse(self, path):
         self._current_path = path
         self._previous_path = None
         self._next_path = None
-        self.extension_list = extension_list
-        self._parse_dir()
+        self.file_list = self._parse_dir()
+
+    def is_first_file(self):
+        file_name = Utility.get_base_name(self.current_path)
+        index = self.file_list.index(file_name)
+        return True if index == 0 else False
+
+    def is_last_file(self):
+        file_name = Utility.get_base_name(self.current_path)
+        index = self.file_list.index(file_name)
+        return True if index == len(self.file_list) - 1 else False
 
     @property
     def current_path(self):
