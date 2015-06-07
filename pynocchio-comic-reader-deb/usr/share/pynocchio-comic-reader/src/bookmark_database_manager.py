b119f63a1abbdca90f72ecf303bfdb049ab3ab62
mstuttgart <michellstut@gmail.com>
2015-06-07 00:48:45 -0300

    deb repo added

---
+++ pynocchio-comic-reader-deb/usr/share/pynocchio-comic-reader/src/bookmark_database_manager.py
@@ -0,0 +1,72 @@
+# coding=UTF-8
+#
+# Copyright (C) 2015  Michell Stuttgart
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from peewee import OperationalError, IntegrityError
+
+from bookmark import Bookmark, BookmarkBaseModel, db
+
+
+class BookmarkManager(BookmarkBaseModel):
+
+    @staticmethod
+    def connect():
+        db.connect()
+        try:
+            db.create_tables([Bookmark], safe=True)
+            print "[INFO] Table 'Bookmark' create/updates sucessfully!"
+        except OperationalError:
+            print "[ERROR] Error to create table 'Bookmark'!"
+
+    @staticmethod
+    def close():
+        db.close()
+        print '[INFO] Bookmark database closed.'
+
+    @staticmethod
+    def add_bookmark(name, path, page, data):
+
+        try:
+            q = Bookmark.insert(comic_name=name, comic_path=path,
+                                comic_page=page, page_data=data)
+            q.execute()
+            print '[INFO] Bookmark %s inserted.' % name
+        except IntegrityError:
+            q = Bookmark.update(comic_page=page, page_data=data).where(
+                Bookmark.comic_path == path)
+            q.execute()
+            print '[INFO] Bookmark updated.'
+
+    @staticmethod
+    def remove_bookmark(path):
+        try:
+            q = Bookmark.delete().where(Bookmark.comic_path == path)
+            q.execute()
+            print '[INFO] Bookmark deleted.'
+        except IntegrityError:
+            print '[ERROR] Bookmark not find.'
+
+    @staticmethod
+    def get_bookmarks(rows_number):
+        query = Bookmark.select().order_by(Bookmark.comic_id.desc()).limit(
+            rows_number)
+        return list(query)
+
+    @staticmethod
+    def get_bookmark_by_path(path):
+        bk_list = Bookmark.select().where(Bookmark.comic_path == path)
+        return bk_list[0] if bk_list else None
+
---
+++ pynocchio-comic-reader-deb/usr/share/pynocchio-comic-reader/src/online_comic_chooser.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015  Michell Stuttgart
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4 import QtGui, QtCore, uic
+
+from parser_factory import ParserFactory
+
+OnlineComicChooserForm, OnlineComicChooserBase = \
+    uic.loadUiType('online_comic_chooser.ui')
+
+
+class OnlineComicChooser(OnlineComicChooserForm, OnlineComicChooserBase):
+
+    def __init__(self, parent=None):
+        super(OnlineComicChooser, self).__init__(parent)
+        self.setupUi(self)
+
+        self.parser = None
+        self.tree_widget.itemDoubleClicked.connect(self.handle_changed)
+        self.host_list_widget.itemClicked.connect(self.item_click)
+
+    def item_click(self, item):
+        print item, str(item.text())
+        self.parser = ParserFactory.create_loader(str(item.text()))
+        self.load_comics()
+
+    def add_parent(self, parent, column, title, data, status_tip):
+        item = QtGui.QTreeWidgetItem(parent, title)
+        item.setData(column, QtCore.Qt.DisplayRole, data)
+        item.setChildIndicatorPolicy(QtGui.QTreeWidgetItem.ShowIndicator)
+        item.setExpanded(True)
+        item.setStatusTip(column, status_tip)
+        item.setIcon(0, QtGui.QIcon(
+            ':icons/elementary3-icon-theme/actions/48/dialog-apply.svg'))
+
+        return item
+
+    def add_child(self, parent, column, title, data, status_tip):
+        item = QtGui.QTreeWidgetItem(parent, title)
+        item.setData(column, QtCore.Qt.DisplayRole, data)
+        item.setStatusTip(column, status_tip)
+        return item
+
+    def handle_changed(self, item, column):
+        if item.parent() is None:
+            self.load_chapter(item, column)
+
+    def load_comics(self):
+
+        for name, url in self.parser.updated_comic_list().items():
+            self.add_parent(
+                self.tree_widget.invisibleRootItem(), 0, name, name, url)
+
+    def load_chapter(self, item, column):
+        chapter_names = self.parser.update_chapters_list(str(item.text(column)))
+
+        for name, url in chapter_names.items():
+            self.add_child(item, column, name, name, url)
