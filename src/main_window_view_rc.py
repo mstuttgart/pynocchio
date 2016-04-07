34d03a5d4b2c18d786c3d6491d5aa1a23ef546a7
mstuttgart <michellstut@gmail.com>
2016-04-07 18:00:55 -0300

    Update resource compiled file

--- src/main_window_view_rc.py
+++ src/main_window_view_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: qui abr 7 15:27:33 2016
+# Created: qui abr 7 17:53:31 2016
 #      by: The Resource Compiler for PySide (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
