f7fb3f9411bb610094eaebaa0327f2d329204803
mstuttgart <michellstut@gmail.com>
2016-04-25 00:10:02 -0300

    Add constructor call in exception class

--- pynocchio_comic_reader/lib/pynocchio_exception.py
+++ pynocchio_comic_reader/lib/pynocchio_exception.py
@@ -19,6 +19,7 @@
 class PynocchioBaseException(Exception):
 
     def __init__(self, msg, *args):
+        super(PynocchioBaseException, self).__init__(msg, *args)
         self.message = msg.format(args)
 
     def __str__(self):
