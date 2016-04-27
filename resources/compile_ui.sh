0a786b1ccaa59e8df66de0eda93f99ffc62f7258
mstuttgart <michellstut@gmail.com>
2016-04-27 02:17:15 -0300

    update views

--- resources/compile_ui.sh
+++ resources/compile_ui.sh
@@ -6,6 +6,5 @@ pyside-uic ui_files/go_to_page_dialog.ui -o ../pynocchio_comic_reader/lib/uic_fi
 pyside-uic ui_files/about_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_about_dialog.py
 pyside-uic ui_files/preference_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_preference_dialog.py
 pyside-uic ui_files/bookmark_manager_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_bookmark_manager_dialog.py
-pyside-uic ui_files/progress_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_progress_dialog.py
 
 pyside-lupdate -verbose ../pynocchio.pro
--- resources/i18n/pynocchio_pt_BR.ts
+++ resources/i18n/pynocchio_pt_BR.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="pt_BR">
+<!DOCTYPE TS><TS version="1.1" language="pt_BR">
 <context>
     <name>AboutDialog</name>
     <message>
