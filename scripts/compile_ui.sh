8a4889021646ce960646b4bb3ddb5c5e03be2ad3
mstuttgart <michellstut@gmail.com>
2016-04-27 14:29:04 -0300

    Scripts folder added

--- scripts/compile_ui.sh
+++ scripts/compile_ui.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-pyside-rcc -o ../pynocchio_comic_reader/lib/uic_files/main_window_view_rc.py main_window_view.qrc
+cd ..
 
-pyside-uic ui_files/main_window_view.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_main_window_view.py
-pyside-uic ui_files/go_to_page_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_go_to_page_dialog.py
-pyside-uic ui_files/about_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_about_dialog.py
-pyside-uic ui_files/preference_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_preference_dialog.py
-pyside-uic ui_files/bookmark_manager_dialog.ui -o ../pynocchio_comic_reader/lib/uic_files/ui_bookmark_manager_dialog.py
+pyside-rcc -verbose -o pynocchio_comic_reader/lib/uic_files/main_window_view_rc.py resources/main_window_view.qrc
 
-pyside-lupdate -verbose ../pynocchio.pro
+pyside-uic resources/ui_files/main_window_view.ui -o pynocchio_comic_reader/lib/uic_files/ui_main_window_view.py
+pyside-uic resources/ui_files/go_to_page_dialog.ui -o pynocchio_comic_reader/lib/uic_files/ui_go_to_page_dialog.py
+pyside-uic resources/ui_files/about_dialog.ui -o pynocchio_comic_reader/lib/uic_files/ui_about_dialog.py
+pyside-uic resources/ui_files/preference_dialog.ui -o pynocchio_comic_reader/lib/uic_files/ui_preference_dialog.py
+pyside-uic resources/ui_files/bookmark_manager_dialog.ui -o pynocchio_comic_reader/lib/uic_files/ui_bookmark_manager_dialog.py
+
+pyside-lupdate -verbose pynocchio.pro
