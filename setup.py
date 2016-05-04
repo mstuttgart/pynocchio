bf3e2d056e45fc36d62311225546ba3b67c74579
mstuttgart <michellstut@gmail.com>
2016-05-04 18:56:38 -0300

    [NEW] Using pyqt_distutils module to compile ui files.

--- setup.py
+++ setup.py
@@ -23,6 +23,14 @@ import re
 import sys
 
 
+try:
+    from pyqt_distutils.build_ui import build_ui
+    cmdclass = {'build_ui': build_ui}
+except ImportError:
+    build_ui = None  # user won't have pyqt_distutils when deploying
+    cmdclass = {}
+
+
 def get_version(package):
     """
     Based in https://github.com/tomchristie/django-rest-framework/blob/
@@ -45,114 +53,9 @@ def get_packages(package):
             for dirpath, dirnames, filenames in os.walk(package)
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
-
-def get_regex_files(path, extension):
-    """
-    :param path Path of ui files
-    :param extension Extension of file. .ui, .qrc, .ts, for example.
-    Return list of files with selected extension .
-    """
-    ret = []
-
-    for dirpath, dirnames, filenames in os.walk(path):
-        ret += [(os.path.join(dirpath, fname), os.path.splitext(fname)[0]) for
-                fname in filenames if os.path.splitext(fname)[1] == extension]
-
-    return ret
-
-
 package_name = 'pynocchio'
 version = get_version(package_name)
 debian_version = '1'
-
-
-class CompileUiFileCommand(distutils.cmd.Command):
-    """
-    A command to compile ui files.
-    """
-
-    description = "Compile PySide ui files"
-
-    # The format is (long option, short option, description).
-    user_options = [
-        ('path=', None, 'The path of ui files folder'),
-    ]
-
-    def initialize_options(self):
-        """
-        Sets the default value for the server socket.
-
-        The method is responsible for setting default values for
-        all the options that the command supports.
-
-        Option dependencies should not be set here.
-        """
-        self.path = 'data'
-
-    def finalize_options(self):
-        """
-        Overriding a required abstract method.
-
-        The method is responsible for setting and checking the
-        final values and option dependencies for all the options
-        just before the method run is executed.
-
-        In practice, this is where the values are assigned and verified.
-        """
-        assert os.path.isdir(self.path), ('[INFO] Folder %s not is valid '
-                                          'folder!' % self.path)
-        assert os.path.lexists(self.path), ('[INFO] Folder %s not exist!' %
-                                            self.path)
-
-    def run(self):
-        print "[INFO] Start compile ui files..."
-
-        uic_folder = 'pynocchio/src/uic_files'
-        files = get_regex_files(self.path, '.ui')
-        for f in files:
-            uic_name = os.path.join(uic_folder, 'ui_' + f[1] + '.py')
-            os.system('pyside-uic %s -d -o %s' % (f[0], uic_name))
-            print '[INFO] Compile %s file' % f[0]
-
-        print '[INFO] uic files added in %s' % uic_folder
-        print '[INFO] Compile ui files successfully!'
-
-        sys.exit()
-
-
-class CompileQrcFileCommand(distutils.cmd.Command):
-    """
-    A command to compile qrc resource files.
-    """
-
-    description = "Compile PySide qrc files"
-
-    # The format is (long option, short option, description).
-    user_options = [
-        ('path=', None, 'The path of qrc files folder'),
-    ]
-
-    def initialize_options(self):
-        self.path = 'data'
-
-    def finalize_options(self):
-        assert os.path.isdir(self.path), ('[INFO] Folder %s not is valid '
-                                          'folder!' % self.path)
-        assert os.path.lexists(self.path), ('[INFO] Folder %s not exist!' %
-                                            self.path)
-
-    def run(self):
-        print "[INFO] Compile qrc files..."
-
-        uic_folder = 'pynocchio/src/uic_files'
-        files = get_regex_files(self.path, '.qrc')
-        for f in files:
-            uic_name = os.path.join(uic_folder, f[1] + '_rc.py')
-            os.system('pyside-rcc -verbose -o %s %s' % (uic_name, f[0]))
-            print '[INFO] Compile %s file' % f[0]
-
-        print '[INFO] rc files added in %s' % uic_folder
-        sys.exit()
 
 
 class CompileProFileCommand(distutils.cmd.Command):
@@ -248,6 +151,8 @@ class BuildDEBPackageCommand(distutils.cmd.Command):
 
         sys.exit()
 
+cmdclass['compile_pro'] = CompileProFileCommand
+cmdclass['build_deb'] = BuildDEBPackageCommand
 
 setup(
     name=package_name,
@@ -264,12 +169,13 @@ setup(
                      'interface.',
     packages=get_packages(package_name),
     test_suite='test',
-    cmdclass={
-        'compile_ui': CompileUiFileCommand,
-        'compile_qrc': CompileQrcFileCommand,
-        'compile_pro': CompileProFileCommand,
-        'build_deb': BuildDEBPackageCommand,
-    },
+    cmdclass=cmdclass,
+    # cmdclass={
+    #     'compile_ui': CompileUiFileCommand,
+    #     'compile_qrc': CompileQrcFileCommand,
+    #     'compile_pro': CompileProFileCommand,
+    #     'build_deb': BuildDEBPackageCommand,
+    # },
     scripts=[
         'pynocchio/pynocchio',
     ],
