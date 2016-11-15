79eefedb2bcffb386121b93264606e98c1bf59d9
mstuttgart <michellstut@gmail.com>
2016-11-15 12:57:31 -0200

    [FIX] Add import in color_line.py

--- pynocchio/color_line.py
+++ pynocchio/color_line.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 
 
 class ColorLine(QtWidgets.QLineEdit):
