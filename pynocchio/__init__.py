b9f740a491ad140691e265c8c250095d0a3733cb
Michell Stuttgart <michellstut@gmail.com>
2017-04-30 15:40:55 -0300

    [NEW] Update version to 2.1.0 and update files

--- pynocchio/__init__.py
+++ pynocchio/__init__.py
@@ -7,5 +7,5 @@ __email__ = 'michellstut@gmail.com'
 __license__ = 'GPL-v3'
 __copyright__ = 'Copyright 2014-2016 Michell Stuttgart Faria'
 __status__ = 'Production'
-__version__ = '2.0.2'
+__version__ = '2.1.0'
 
