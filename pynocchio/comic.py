75b82627e8867cb946e77514c24e8277e5ef3597
Michell Stuttgart <michellstut@gmail.com>
2017-05-24 07:57:03 -0300

    [NEW] Add doc to Page class

--- pynocchio/comic.py
+++ pynocchio/comic.py
@@ -49,6 +49,14 @@ class Page:
     """This is basic class of Pynocchio. Represents a comic page object"""
 
     def __init__(self, data, title, number):
+        """
+        Comic Page class __init__ method
+
+        Args:
+            data (bin): comic page binary data
+            title (str): page title
+            number (int): page number
+        """
         self.data = data
         self.title = title
         self.number = number
