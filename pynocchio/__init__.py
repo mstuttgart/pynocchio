19faf2fa0352b8ac5b86f9943a3b6aea3d2e917b
Michell Stuttgart <michellstut@gmail.com>
2017-08-12 14:46:03 -0300

    [NEW] Add info about file in __version__

--- pynocchio/__init__.py
+++ pynocchio/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-__title__ = 'Pynocchio'
-__author__ = 'Michell Stuttgart Faria'
-__maintainer__ = 'Michell Stuttgart Faria'
-__email__ = 'michellstut@gmail.com'
-__license__ = 'GPL-v3'
-__copyright__ = 'Copyright 2014-2016 Michell Stuttgart Faria'
-__status__ = 'Production'
-__version__ = '2.1.0'
+from .__version__ import (__title__,  # noqa: F401
+                          __description__,
+                          __version__,
+                          __author__,
+                          __author_email__,
+                          __maintainer__,
+                          __maintainer_email__,
+                          __url__,
+                          __download_url__,
+                          __copyright__,
+                          __license__)
---
+++ pynocchio/__version__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'Pynocchio'
+__description__ = 'Pynocchio is a image viewer specialized in comic reading'
+__version__ = '2.1.0'
+__url__ = 'https://github.com/pynocchio/pynocchio'
+__download_url__ = 'https://github.com/pynocchio/pynocchio/releases'
+__author__ = 'Michell Stuttgart'
+__author_email__ = 'michellstut@gmail.com'
+__maintainer__ = 'Michell Stuttgart'
+__maintainer_email__ = 'michellstut@gmail.com'
+__license__ = 'GPL-v3 License'
+__copyright__ = 'Copyright 2014-2017 Michell Stuttgart'
+__status__ = 'Production'
