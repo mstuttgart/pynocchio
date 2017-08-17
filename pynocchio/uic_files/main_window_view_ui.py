743c6f160aac7d38ffe666f237b79e1414fe1396
Michell Stuttgart <michellstut@gmail.com>
2017-08-17 12:07:20 -0300

    [FIX] Error in read database file

--- pynocchio/uic_files/main_window_view_ui.py
+++ pynocchio/uic_files/main_window_view_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file './forms/main_window_view.ui'
 #
-# Created by: PyQt5 UI code generator 5.8.2
+# Created by: PyQt5 UI code generator 5.9
 #
 # WARNING! All changes made in this file will be lost!
 
