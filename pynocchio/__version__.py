032a5f2fa557db5fadce1ab3bfe9af4467fb3b91
Michell Stuttgart <michellstut@gmail.com>
2017-08-17 00:09:22 -0300

    [NEW] Update version to 2.2.0

--- pynocchio/__version__.py
+++ pynocchio/__version__.py
@@ -2,7 +2,7 @@
 
 __title__ = 'Pynocchio'
 __description__ = 'Pynocchio is a image viewer specialized in comic reading'
-__version__ = '2.1.1'
+__version__ = '2.2.0'
 __url__ = 'https://github.com/pynocchio/pynocchio'
 __download_url__ = 'https://github.com/pynocchio/pynocchio/releases'
 __author__ = 'Michell Stuttgart'
