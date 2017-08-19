95dbaee9e6b6b6f9a049bf33fa91cfee7261a95c
Michell Stuttgart <michellstut@gmail.com>
2017-08-18 22:33:49 -0300

    [REF] Adjust in app name

--- pynocchio/__version__.py
+++ pynocchio/__version__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__title__ = 'Pynocchio'
+__title__ = 'pynocchio'
 __description__ = 'Pynocchio is a image viewer specialized in comic reading'
 __version__ = '2.2.1'
 __url__ = 'https://github.com/pynocchio/pynocchio'
