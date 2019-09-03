b481d8473d53972e489077d22aae0149141e03b3
Michell Stuttgart <michellstut@gmail.com>
2019-09-03 19:30:34 -0300

    Update translations

--- i18n/en-US.ts
+++ i18n/en-US.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="en_US" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
 <context>
     <name>AboutDialog</name>
     <message>
