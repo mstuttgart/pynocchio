474b2688d29bbe600fc0666dcd89def8a4fcc38c
Michell Stuttgart <michellstut@gmail.com>
2021-10-12 15:54:00 -0300

    Update ui files and fix version

--- pynocchio/__version__.py
+++ pynocchio/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'pynocchio'
 __description__ = 'Pynocchio is a image viewer specialized in comic reading'
-__version__ = '3.2.0'
+__version__ = '3.2.1'
 __url__ = 'https://github.com/pynocchio/pynocchio'
 __download_url__ = 'https://github.com/pynocchio/pynocchio/releases'
 __author__ = 'Michell Stuttgart'
